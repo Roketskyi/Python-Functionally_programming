8e3c64c3dccd7552bc415bdefee4a15c63f4a647
Roman <romanroketskiy05@gmail.com>
2024-03-19 12:56:09 +0200

    new code

--- main1.py
+++ main1.py
@@ -2,7 +2,7 @@ def process_numbers():
     num1 = float(input("Введіть перше число: "))
     num2 = float(input("Введіть друге число: "))
     num3 = float(input("Введіть третє число: "))
-
+    
     def square_or_fourth_power(num):
         if num >= 0:
             return num ** 2
@@ -12,7 +12,7 @@ def process_numbers():
     result1 = square_or_fourth_power(num1)
     result2 = square_or_fourth_power(num2)
     result3 = square_or_fourth_power(num3)
-
+    
     print("Результати піднесення до квадрата або четвертої ступеня:")
     print("Число 1:", result1)
     print("Число 2:", result2)
